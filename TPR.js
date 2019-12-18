b7045ddb05b92d5f7cc82793d826a2a02805810a
Kobby Adu <kobby129@gmail.com>
2019-12-18 07:47:05 -0700

    final commit

---
+++ TPR.js
@@ -0,0 +1,15 @@
+<body>
+
+<script>
+function myFunction11() {
+  var checkbox = document.getElementById('dynamics');
+  var text = document.getElementById("text11");
+  if (checkbox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+</script>
+<p id="text11" style="display:none">Mark your music so that you remember to play a certain dynamic, then practice playing that dynamic. In fact, practicing playing quietly is a great way to improve control over your instrument and tone quality </p>
+</body>
